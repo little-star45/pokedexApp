d27294cb907febcbaf0fcaaae7aa6a56d5853ad0
PatrycjaD <domanska.patrycja7@gmail.com>
2025-07-21 00:00:54 +0200

    changehome link

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import PokemonCard from './pokemonCard'
 import { Navbar, Nav } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from 'react-bootstrap'
 import balloonIcon from './pokemon-1513925_1920.jpg'
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NaviBar = () => {
   return (
@@ -19,7 +18,9 @@ const NaviBar = () => {
             />{' '}
            <Navbar.Brand style={{fontWeight: '800', fontSize: '24px'}}> Pokédex App </Navbar.Brand>
            <Nav className="me-auto">
-             <Nav.Link style={{fontWeight: '700', fontSize: '20px'}} href="/"> Home </Nav.Link>
+             <LinkContainer to="/">
+              <Nav.Link style={{fontWeight: '700', fontSize: '20px'}}>Home</Nav.Link>
+            </LinkContainer>
            </Nav>
         </Container>
     </Navbar>
